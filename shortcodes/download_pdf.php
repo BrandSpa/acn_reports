a54bbd0dcb5a1e743aaf63e0f840274768ed4b16
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2017-12-19 13:59:39 -0500

    Se realiza ajuste en el shortcode

--- shortcodes/download_pdf.php
+++ shortcodes/download_pdf.php
@@ -14,7 +14,7 @@ function bs_download_pdf_sc($atts, $content = null) {
   ];
 
   $at = shortcode_atts( $attributes , $atts );
-
+  print_r(at);
 	$props = [
 		'btn' => [
 			'text' => $at['btn_text'],
