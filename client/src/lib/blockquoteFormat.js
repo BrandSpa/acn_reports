c9b35b5b7afd1f0bb000540762a6fe6883df0325
Michael Sanchez <michaelsanchez@brandspa.com>
2018-01-09 12:13:40 -0500

    Add fn to jquery

--- client/src/lib/blockquoteFormat.js
+++ client/src/lib/blockquoteFormat.js
@@ -1,8 +1,8 @@
 function blockquote() {
   $('blockquote').each(function(){
-    let text = this.childNodes[0].nodevalue;
-    text = "»"+text+"«";
+    let text = $(this).justtext();
     let paragraph = document.createElement('p');
+    text = "»"+text+"«";
     paragraph.innerText = text;
 
     $(this).html(paragraph);
@@ -11,5 +11,12 @@ function blockquote() {
 }
 
 export default function blockquoteFormat() {
+  $.fn.justtext = function(){
+    return $(this).clone()
+            .children()
+            .remove()
+            .end()
+            .text();
+  }
   $(document).on('ready', blockquote());
 }
