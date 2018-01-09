8873ffd91bdf47f9bb66f7b0e6fea5f6f0618352
Michael Sanchez <michaelsanchez@brandspa.com>
2018-01-09 11:59:57 -0500

    Bug fixed

--- client/src/lib/blockquoteFormat.js
+++ client/src/lib/blockquoteFormat.js
@@ -1,7 +1,11 @@
 function blockquote() {
   $('blockquote').each(function(){
-    let text = $(this).text();
-    text = ">>"+text+"<<";
+    let text = $(this).clone()    //clone the element
+    .children() //select all the children
+    .remove()   //remove all the children
+    .end()  //again go back to selected element
+    .text();    //get the text of element
+    text = "»"+text+"«";
     let paragraph = document.createElement('p');
     paragraph.innerText = text;
 
