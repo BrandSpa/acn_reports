905979d9facc0738219f3215a8a18ff71c486b5a
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-01-15 12:01:25 -0500

    remove image in banner

--- templates/post_banner.php
+++ templates/post_banner.php
@@ -1,3 +1,2 @@
 <div class="bs-post__header--image" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>)">
-	<img src="<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>" alt="<?php the_title() ?>">
 </div>
