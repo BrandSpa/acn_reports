d8a37c1acec350889184711db5cf63a1a69113f4
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-01-16 17:36:25 -0500

    ajustes

--- templates/post_banner.php
+++ templates/post_banner.php
@@ -1,3 +1,3 @@
 <div class="bs-post__header--image" style="background-image: url(<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>); background-size:100%; background-repeat:no-repeat">
-    <img src="<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>" alt="" style="max-width: 100%">
+    <img src="<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>" alt="" style="max-width: 100%; visibility:hidden">
 </div>
