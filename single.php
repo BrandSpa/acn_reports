5ab896034e934d10d959dac231ff4291b1b13756
Michael Sanchez <michaelsanchez@MacBook-Air-de-Michael.local>
2018-01-16 17:19:55 -0500

    image width

--- single.php
+++ single.php
@@ -33,7 +33,7 @@
                 <?php require('templates/post_header.php') ?>
                 <?php endif; ?>
             </header>
-            <div class="visible-xs">
+            <div class="visible-xs post-image ">
                 <img src="<?php echo get_the_post_thumbnail_url($post->ID, 'full') ?>" alt="<?php the_title(); ?>">
             </div>
             <div class="content">
